929fa85ec5671bd62f034990377189b082c5c2bb
Tóth Dániel <tothdani562@proton.me>
2026-04-05 10:57:57 +0200

    Final commit

--- init.sql
+++
@@ -1,9 +0,0 @@
--- TaskTracker Database Initialization Script
--- Ez a fájl automatikusan fut, amikor a PostgreSQL konténer elindul
-
--- Alapvető extension-ök engedélyezése
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
--- Tábla létrehozása később az EF Core Migration-eken keresztül történik
--- Ez a fájl jelenleg csak az extension-öket inicializálja
